83c2ee1b3eddd80869574c87029f383872707fa2
Nicolas Diaz <diaz.nicolasandres@gmail.com>
2025-11-19 20:14:15 -0300

    Roles

---
+++ src/auth/guards/any-auth.guard.ts
@@ -0,0 +1,32 @@
+import { Injectable, ExecutionContext } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from '../../common/decorators/public.decorator';
+
+/**
+ * Guard que acepta autenticación tanto por JWT (usuarios Google)
+ * como por LDAP (dashboard administración).
+ *
+ * Se comporta como los demás guards respetando el decorador @Public().
+ */
+@Injectable()
+export class AnyAuthGuard extends AuthGuard(['jwt', 'ldap']) {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
+    return super.canActivate(context);
+  }
+}
+
+
